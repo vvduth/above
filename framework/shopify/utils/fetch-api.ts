ab52941a7168294fc27c6c58c5fa48b6f6f46df4
Duc Thai <ducthai060501@gmail.com>
2022-08-04 12:52:19 +0300

    add types

--- framework/shopify/utils/fetch-api.ts
+++ framework/shopify/utils/fetch-api.ts
@@ -1,21 +1,23 @@
-
 type FetchParam = {
-    query : string
-}
-const fetchApi = async ({query}:FetchParam) => {
-    const url = "http://localhost:4000/graphql"
+  query: string;
+};
+const fetchApi = async ({ query }: FetchParam) => {
+  const url = "http://localhost:4000/graphql";
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body : JSON.stringify({
-            query: query
-        })
-    })
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: query,
+    }),
+  });
 
-    const data  = await response.json() ; 
-    return {data}
-}
-export default fetchApi ;
+  const { data, error } = await response.json();
+  if (error) {
+    throw new Error(error[0].message ?? error.message);
+  }
+  return { data };
+};
+export default fetchApi;
