17cab9a8734f4beda3b4790562952ec69ad7356e
Duc Thai <ducthai060501@gmail.com>
2022-08-07 23:14:05 +0300

    done homepage

--- components/ui/Sidebar/Sidebar.tsx
+++ components/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,9 @@
 
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
+import {
+  disableBodyScroll,
+  enableBodyScroll, clearAllBodyScrollLocks
+} from 'body-scroll-lock'
 
 interface Props {
   children: any
@@ -7,15 +11,28 @@ interface Props {
   onClose: () => void ;
 }
 
-const Sidebar: FC<Props> = ({ children, isOpen }: Props) => {
-
+const Sidebar: FC<Props> = ({ children, isOpen, onClose }: Props) => {
+  const ref = useRef() as React.MutableRefObject<HTMLDivElement>; // return the html conponent when the component is rendered
+  
+  useEffect(()=>{
+    if (ref.current) {
+      if(isOpen) {
+        disableBodyScroll(ref.current)
+      } else {
+        enableBodyScroll(ref.current)
+      }
+    }
+    return () => {
+      clearAllBodyScrollLocks()
+    }
+  },[isOpen])
   return (
     <>
       { isOpen ? (
         <div className="fixed inset-0 overflow-hidden h-full z-50">
           <div className="absolute inset-0 overflow-hidden">
             <div
-              onClick={() => {}}
+              onClick={() => {onClose()}}
               className="absolute inset-0 bg-black bg-opacity-50 transition-opacity"
             />
             <section className="absolute inset-y-0 right-0 pl-10 max-w-full flex sm:pl-16 outline-none">
--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,48 +1,70 @@
-import { createContext, FC, useContext, useState } from "react"
+import { createContext, FC, useContext, useReducer, useMemo } from "react";
 
 export interface StateModifiers {
-  openSidebar: () => void
-  closeSidebar: () => void
+  openSidebar: () => void;
+  closeSidebar: () => void;
 }
 
 export interface StateValues {
-  isSidebarOpen: boolean
+  isSidebarOpen: boolean;
 }
 
 const stateModifiers = {
   openSidebar: () => {},
-  closeSidebar: () => {}
-}
+  closeSidebar: () => {},
+};
 
-const initialState = { isSidebarOpen: false }
+const initialState = { isSidebarOpen: false };
 
-type State = StateValues & StateModifiers
+type State = StateValues & StateModifiers;
 
 const UIContext = createContext<State>({
   ...stateModifiers,
-  ...initialState
-})
+  ...initialState,
+});
 
- const UIProvider: FC = ({children}:any) => {
+type Action = {
+  type: "OPEN_SIDEBAR" | "CLOSE_SIDEBAR";
+};
 
-  const openSidebar = () => alert("Opening Sidebar!")
-  const closeSidebar = () => alert("Closing Sidebar!")
-
-  const value = {
-    openSidebar,
-    closeSidebar,
-    isSidebarOpen: true
+function uiReducer(state: StateValues, actions: Action) {
+  switch (actions.type) {
+    case "OPEN_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: true,
+      };
+    }
+    case "CLOSE_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: false,
+      };
+    }
   }
-
-  return (
-    <UIContext.Provider value={value}>
-      {children}
-    </UIContext.Provider>
-  )
 }
+
+const UIProvider: FC = ({ children }: any) => {
+  const [state, dispatch] = useReducer(uiReducer, initialState);
+
+  const openSidebar = () => dispatch({ type: "OPEN_SIDEBAR" });
+  const closeSidebar = () => dispatch({ type: "CLOSE_SIDEBAR" });
+
+  const value = useMemo(()=> {
+    return {
+      ...state,
+      openSidebar,
+      closeSidebar
+    }
+  },[state.isSidebarOpen])
+
+  
+
+  return <UIContext.Provider value={value}>{children}</UIContext.Provider>;
+};
 
 export const useUI = () => {
-  const context = useContext(UIContext)
-  return context
-}
-export default UIProvider
+  const context = useContext(UIContext);
+  return context;
+};
+export default UIProvider;
