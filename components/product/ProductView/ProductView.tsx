4d16d0775905eaeed5d8c84527979e917d1d21f3
Duc Thai <ducthai060501@gmail.com>
2022-08-09 00:32:11 +0300

    dispaly variant options

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,23 +1,20 @@
-
-import cn from 'classnames'
-import { FC } from 'react'
-import s from './ProductView.module.css'
-import { Container, Button } from '@components/ui'
-import Image from "next/image"
-import ProductSlider from '../ProductSlider'
-import { Product } from '@common/types/product'
-
+import cn from "classnames";
+import { FC, JSXElementConstructor, ReactElement, ReactFragment, ReactPortal } from "react";
+import s from "./ProductView.module.css";
+import { Container, Button } from "@components/ui";
+import Image from "next/image";
+import ProductSlider from "../ProductSlider";
+import { Product } from "@common/types/product";
 
 interface Props {
-  product: Product
+  product: Product;
 }
 
 const ProductView: FC<Props> = ({ product }) => {
-
   return (
     <Container>
-      <div className={cn(s.root, 'fit', 'mb-5')}>
-        <div className={cn(s.productDisplay, 'fit')}>
+      <div className={cn(s.root, "fit", "mb-5")}>
+        <div className={cn(s.productDisplay, "fit")}>
           <div className={s.nameBox}>
             <h1 className={s.name}>{product.name}</h1>
             <div className={s.price}>
@@ -27,43 +24,48 @@ const ProductView: FC<Props> = ({ product }) => {
             </div>
           </div>
           <ProductSlider>
-            {product.images.map(image => 
-            <div key= {Math.random()}className={s.imageContainer}>
-              <Image
-                className={s.img}
-                src={image.url}
-                alt={image.alt}
-                width={1050}
-                height={1050}
-                quality="85"
-              />
-            </div>)}
-            
+            {product.images.map((image) => (
+              <div key={Math.random()} className={s.imageContainer}>
+                <Image
+                  className={s.img}
+                  src={image.url}
+                  alt={image.alt}
+                  width={1050}
+                  height={1050}
+                  quality="85"
+                />
+              </div>
+            ))}
           </ProductSlider>
         </div>
         <div className={s.sidebar}>
           <section>
-            <div className="pb-4">
-              <h2 className="uppercase font-medium">Color</h2>
-              <div className="flex flex-row py-4">
-                Variant Options Here!
+            {product.options.map((option) => (
+              <div key={product.id} className="pb-4">
+                <h2 className="uppercase font-medium">{option.displayName}</h2>
+                <div className="flex flex-row py-4">
+                  { option.values.map((optValue: { label: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) =>
+                      <div key={`${option.id}-${optValue.label}`}>
+                        {optValue.label}
+                      </div>
+                    )}
+                </div>
               </div>
-            </div>
+            ))}
+
             <div className="pb-14 break-words w-full max-w-xl text-lg">
               {product.description}
             </div>
           </section>
           <div>
-            <Button
-            className={s.button}
-            onClick={()=> alert("Adding brooo")}>
+            <Button className={s.button} onClick={() => alert("Adding brooo")}>
               Add to Cart
             </Button>
           </div>
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductView
+export default ProductView;
